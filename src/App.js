f3276a50fbb012e7e275f7f80f3ea547dedefdad
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-08-11 18:03:27 +0530

    Day-6: Loading the application - When the user clicks on the button. (Set isLoading = true). While fetching the data from the backend. show a loader on the screen up till the time data doesn't come. Once the data has come remove the loader (Set isLoading = false)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
 
   const fetchMovieHandler = async () => {
     console.log("fetch movie")
     try {
+
+      setIsLoading(true);
       const response = await fetch('https://swapi.dev/api/films/');
 
       const data = await response.json();
@@ -23,20 +26,22 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+      setIsLoading(false);
     }
     catch {
       //
+      //setIsLoading(true);
     }
 
   }
 
-  //Fetch movies data when page is reload
+  //Fetch movies data when page is reload using useEffect
   // useEffect(() => {
   //   console.log("movie is fetched")
   //   fetchMovieHandler();
   // }, []);
 
-
+  //By fetch method
   // const fetchMovieHandler = () => {
   //   fetch('https://swapi.dev/api/films/').then((response) => {
   //     return response.json();
@@ -55,12 +60,15 @@ function App() {
 
   return (
     <React.Fragment>
+      {isLoading && <section>
+        <h2>Please Wait while data is Loading ...</h2>
+      </section>}
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
+      {!isLoading && <section>
         <MoviesList movies={movies} />
-      </section>
+      </section>}
     </React.Fragment>
   );
 }
